e02a9eac165cc8932c25da8c6c99627e67319091
Robzlegz <robzlegz@gmail.com>
2021-02-13 21:06:02 +0200

    getting month

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import BASE_URL from './api';
 import './App.css';
 
 const App = () => {
-
-  console.log(BASE_URL)
-
   return (
     <div className="app">
       
--- src/api.js
+++ src/api.js
@@ -1,5 +1,11 @@
 import API_KEY from "./apikey";
 
-const BASE_URL = `GET https://api.rawg.io/api/games?key=${API_KEY}`;
-
-export default BASE_URL;
+const BASE_URL = `https://api.rawg.io/api/games?key=${API_KEY}`;
+const getMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if(month < 10){
+        return `0${month}`;
+    }else{
+        return month;
+    }
+}
