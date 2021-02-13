0cb9c4f4864c4199e46d5e7335a06ab86867699e
Robzlegz <robzlegz@gmail.com>
2021-02-13 21:20:45 +0200

    creating api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import API_KEY from "./apikey";
 
-const BASE_URL = `https://api.rawg.io/api/games?key=${API_KEY}`;
+const BASE_URL = `https://api.rawg.io/api/`;
+const KEY = `key=${API_KEY}`;
+const gamesPerPage = 10;
 
 const getMonth = () => {
     const month = new Date().getMonth() + 1;
@@ -23,6 +25,11 @@ const getDay = () => {
 const currentYear = new Date().getFullYear();
 const currentMonth = getMonth();
 const currentDay = getDay();
+
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=${gamesPerPage}`;
+
+const popularGamesURL = () => `${BASE_URL}${popular_games}${KEY}`;
