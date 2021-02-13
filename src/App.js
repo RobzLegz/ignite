0721786b1f6456dd606ad45edee81fee3052b7a6
Robzlegz <robzlegz@gmail.com>
2021-02-13 21:01:38 +0200

    generating api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import BASE_URL from './api';
 import './App.css';
 
 const App = () => {
+
+  console.log(BASE_URL)
+
   return (
     <div className="app">
       
---
+++ src/api.js
@@ -0,0 +1,5 @@
+import API_KEY from "./apikey";
+
+const BASE_URL = `GET https://api.rawg.io/api/games?key=${API_KEY}`;
+
+export default BASE_URL;
