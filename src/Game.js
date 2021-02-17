b97b52afa859b9f8d0829b9e7fd6fe6d866a75e2
Robzlegz <robzlegz@gmail.com>
2021-02-17 15:31:15 +0200

    adding styled components

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,28 @@
+import { motion } from 'framer-motion';
 import React from 'react'
+import styled from 'styled-components';
 
 const Game = ({name,released,id,image}) => {
     return (
-        <div>
+        <StyledGame>
             <h3>{name}</h3>
             <p>{released}</p>
             <img src={image} alt={name}/>
-        </div>
+        </StyledGame>
     )
 }
 
-export default Game
+const StyledGame = styled(motion.div)`
+    min-height:30vh;
+    box-shadow: 0px 5px 25px rgba(0,0,0,0.3);
+    text-align: center;
+    border-radius: 30px;
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 40vh;
+        object-fit:cover;
+    }
+`;
+
+export default Game;
