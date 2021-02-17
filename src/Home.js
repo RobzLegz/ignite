a82b4017eccc1f921421f579d7d3fb3c3153cb60
Robzlegz <robzlegz@gmail.com>
2021-02-17 15:17:58 +0200

    generating games

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
+import { motion } from 'framer-motion';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { loadGames } from './actions/gamesAction';
 import Game from './Game';
 
@@ -7,15 +9,26 @@ const Home = () => {
 
     const dispatch = useDispatch();
 
+    const {popular,newGames,upcoming} = useSelector((state) => state.games);
+
     useEffect(() => {
         dispatch(loadGames());
-    }, []);
+    }, [dispatch]);
 
     return (
-        <div className="home">
-            <Game />
-        </div>
+        <Gamelist className="home">
+            <h1>Upcoming Games</h1>
+            <Games>
+                {upcoming.map((game) => (
+                    <Game />
+                ))}
+            </Games>
+        </Gamelist>
     )
 }
+
+const Gamelist = styled(motion.div)``;
+
+const Games = styled(motion.div)``;
 
 export default Home
